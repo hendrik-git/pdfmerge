21a9a082a04ef11e36cdd701126770b85b4783bb
Hendrik Pöttker <hendrik.poettker@web.de>
2023-09-09 19:21:30 +0200

    Added color to error message(s) in the console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use colored::*;
+
 fn main() {
     if let Err(e) = pdfmerge::get_args().and_then(pdfmerge::run) {
-        eprintln!("{}", e);
+        eprintln!("{} {}", "Error:".red(), e);
         std::process::exit(1);
     }
 }
