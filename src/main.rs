31c2e2fb10b420ddacad86e67502ebc465c0edc9
Hendrik Pöttker <hendrik.poettker@web.de>
2023-09-10 23:41:36 +0200

    Fix linux workflow error with conditional compilation

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use colored::*;
 
 fn main() {
     // necessary to have Windows display color correctly with the standalone executable
-    if cfg!(target_os = "windows") {
-        control::set_virtual_terminal(true).unwrap();
-    }
+    fix_color_output();
 
     if let Err(e) = pdfmerge::get_args().and_then(pdfmerge::run) {
         eprintln!("{} {}", "Error:".red(), e);
@@ -12,3 +10,12 @@ fn main() {
     }
     println!("{}", "Done!".green());
 }
+
+
+#[cfg(target_os = "windows")]
+fn fix_color_output() {
+    control::set_virtual_terminal(true).unwrap();
+}
+
+#[cfg(not(target_os = "windows"))]
+fn fix_color_output() {}
