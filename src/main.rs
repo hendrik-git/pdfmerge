076f27a6b5d52830309d3108935c7f987c6098f6
Hendrik Pöttker <hendrik.poettker@web.de>
2023-09-10 23:33:02 +0200

    Added conditional for windows compilation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use colored::*;
 
 fn main() {
     // necessary to have Windows display color correctly with the standalone executable
-    control::set_virtual_terminal(true).unwrap();
+    if cfg!(target_os = "windows") {
+        control::set_virtual_terminal(true).unwrap();
+    }
 
     if let Err(e) = pdfmerge::get_args().and_then(pdfmerge::run) {
         eprintln!("{} {}", "Error:".red(), e);
