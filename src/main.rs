778100b9867f2d485a5754972a4c6b86bbcda545
Hendrik Pöttker <hendrik.poettker@web.de>
2023-09-10 20:16:24 +0200

    Added fix for color output not displaying on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use colored::*;
 
 fn main() {
+    // necessary to have Windows display color correctly with the standalone executable
+    control::set_virtual_terminal(true).unwrap();
+
     if let Err(e) = pdfmerge::get_args().and_then(pdfmerge::run) {
         eprintln!("{} {}", "Error:".red(), e);
         std::process::exit(1);
     }
+    println!("{}", "Done!".green());
 }
